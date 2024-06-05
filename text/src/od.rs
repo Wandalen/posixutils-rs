72fb827c60fc2b9772f177417eab26044f1437ca
Denys Levchenko <denys.l@obox.systems>
2024-06-05 13:46:58 +0300

    add args

--- text/src/od.rs
+++ text/src/od.rs
@@ -1,58 +1,106 @@
-use std::env;
 use std::fs::File;
-use std::io::{self, Read};
-use std::path::Path;
+use std::io::{self, BufReader, Read};
+use std::path::PathBuf;
 
-fn print_hex_dump<R: Read>(reader: &mut R) -> io::Result<()> {
-    let mut buffer = [0; 16];
-    let mut offset = 0;
+use clap::Parser;
+use gettextrs::{bind_textdomain_codeset, textdomain};
+use plib::PROJECT_NAME;
 
-    while let Ok(n) = reader.read(&mut buffer) {
-        if n == 0 {
-            break;
-        }
+/// Hex, octal, ASCII, and other types of dumps
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about)]
+struct Args {
+    /// Address base (d for decimal, o for octal, x for hexadecimal, n for none)
+    #[arg(short = 'A')]
+    address_base: Option<char>,
 
-        print!("{:08x}  ", offset);
-        for i in 0..16 {
-            if i < n {
-                print!("{:02x} ", buffer[i]);
-            } else {
-                print!("   ");
+    /// Skip bytes from the beginning of the input
+    #[arg(short = 'j')]
+    skip: Option<String>,
+
+    /// Read only the specified number of bytes
+    #[arg(short = 'N')]
+    count: Option<String>,
+
+    /// Select the output format
+    #[arg(short = 't')]
+    type_string: Option<String>,
+
+    /// Verbose output
+    #[arg(short = 'v')]
+    verbose: bool,
+
+    /// Input files
+    #[arg(required = true)]
+    files: Vec<PathBuf>,
+}
+
+impl Args {
+    fn validate_args(&mut self) -> Result<(), String> {
+        // Check if conflicting options are used together
+
+        Ok(())
+    }
+}
+
+fn od(args: &Args) -> io::Result<()> {
+    for file in &args.files {
+        let path = PathBuf::from(file);
+        let file = File::open(path)?;
+        let mut reader = BufReader::new(file);
+
+        let mut buffer = [0; 16];
+        let mut offset = 0;
+
+        while let Ok(n) = reader.read(&mut buffer) {
+            if n == 0 {
+                break;
             }
 
-            if i == 7 {
-                print!(" ");
+            print!("{:08x}  ", offset);
+            for i in 0..16 {
+                if i < n {
+                    print!("{:02x} ", buffer[i]);
+                } else {
+                    print!("   ");
+                }
+
+                if i == 7 {
+                    print!(" ");
+                }
             }
-        }
 
-        print!(" |");
+            print!(" |");
 
-        for i in 0..n {
-            let c = buffer[i];
-            if c.is_ascii_graphic() || c == b' ' {
-                print!("{}", c as char);
-            } else {
-                print!(".");
+            for i in 0..n {
+                let c = buffer[i];
+                if c.is_ascii_graphic() || c == b' ' {
+                    print!("{}", c as char);
+                } else {
+                    print!(".");
+                }
             }
+
+            println!("|");
+
+            offset += n;
         }
-
-        println!("|");
-
-        offset += n;
     }
 
     Ok(())
 }
 
-fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        eprintln!("Usage: {} <file>", args[0]);
-        return Ok(());
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    textdomain(PROJECT_NAME)?;
+    bind_textdomain_codeset(PROJECT_NAME, "UTF-8")?;
+    let mut args = Args::parse();
+    args.validate_args()?;
+    let mut exit_code = 0;
+
+    if let Err(err) = od(&args) {
+        exit_code = 1;
+        eprint!("{}", err);
     }
 
-    let path = Path::new(&args[1]);
-    let mut file = File::open(&path)?;
-
-    print_hex_dump(&mut file)
+    std::process::exit(exit_code)
 }
