643448b6f2213c7889c1c26d31d54fd8b8525a24
Dmytro Kriuchkov <kriuchkovdima@gmail.com>
2024-07-23 10:39:47 +0300

    [timeout] Signal name parsing

--- process/timeout.rs
+++ process/timeout.rs
@@ -18,6 +18,77 @@ use std::{error::Error, time::Duration};
 
 const DEFAULT_ERROR_EXIT_STATUS: i32 = 125;
 
+#[cfg(target_os = "macos")]
+const SIGLIST: [(&str, u32); 31] = [
+    ("HUP", 1),
+    ("INT", 2),
+    ("QUIT", 3),
+    ("ILL", 4),
+    ("TRAP", 5),
+    ("ABRT", 6),
+    ("EMT", 7),
+    ("FPE", 8),
+    ("KILL", 9),
+    ("BUS", 10),
+    ("SEGV", 11),
+    ("SYS", 12),
+    ("PIPE", 13),
+    ("ALRM", 14),
+    ("TERM", 15),
+    ("URG", 16),
+    ("STOP", 17),
+    ("TSTP", 18),
+    ("CONT", 19),
+    ("CHLD", 20),
+    ("TTIN", 21),
+    ("TTOU", 22),
+    ("IO", 23),
+    ("XCPU", 24),
+    ("XFSZ", 25),
+    ("VTALRM", 26),
+    ("PROF", 27),
+    ("WINCH", 28),
+    ("INFO", 29),
+    ("USR1", 30),
+    ("USR2", 31),
+];
+
+#[cfg(target_os = "linux")]
+const SIGLIST: [(&str, u32); 32] = [
+    ("HUP", 1),
+    ("INT", 2),
+    ("QUIT", 3),
+    ("ILL", 4),
+    ("TRAP", 5),
+    ("ABRT", 6),
+    ("IOT", 6),
+    ("BUS", 7),
+    ("FPE", 8),
+    ("KILL", 9),
+    ("USR1", 10),
+    ("SEGV", 11),
+    ("USR2", 12),
+    ("PIPE", 13),
+    ("ALRM", 14),
+    ("TERM", 15),
+    ("STKFLT", 16),
+    ("CHLD", 17),
+    ("CONT", 18),
+    ("STOP", 19),
+    ("TSTP", 20),
+    ("TTIN", 21),
+    ("TTOU", 22),
+    ("URG", 23),
+    ("XCPU", 24),
+    ("XFSZ", 25),
+    ("VTALRM", 26),
+    ("PROF", 27),
+    ("WINCH", 28),
+    ("IO", 29),
+    ("PWR", 30),
+    ("SYS", 31),
+];
+
 /// timeout — execute a utility with a time limit
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about)]
@@ -32,14 +103,14 @@ struct Args {
 
     /// Send a SIGKILL signal if the child process created to execute the utility has not terminated after the time period
     /// specified by time has elapsed since the first signal was sent. The value of time shall be interpreted as specified for
-    /// the [duration](Args::duration) operand.
+    /// the duration operand.
     #[arg(short = 'k', long, value_parser = parse_duration)]
     kill_after: Option<Duration>,
 
     /// Specify the signal to send when the time limit is reached, using one of the symbolic names defined in the <signal.h> header.
     /// Values of signal_name shall be recognized in a case-independent fashion, without the SIG prefix. By default, SIGTERM shall be sent.
-    #[arg(short = 's', long)]
-    signal: Option<String>,
+    #[arg(short = 's', long, default_value = "TERM", value_parser = parse_signal)]
+    signal: u32,
 
     /// The maximum amount of time to allow the utility to run, specified as a decimal number with an optional decimal fraction and an optional suffix.
     #[arg(name = "DURATION", value_parser = parse_duration)]
@@ -85,16 +156,48 @@ fn parse_duration(s: &str) -> Result<Duration, String> {
     }
 }
 
+/// Parses and validates the signal name, returning its integer value.
+///
+/// # Arguments
+///
+/// * `s` - [str](std::str) that represents the signal name.
+///
+/// # Errors
+///
+/// Returns an error if passed invalid input.
+///
+/// # Returns
+///
+/// Returns the integer value of the signal.
+fn parse_signal(s: &str) -> Result<u32, String> {
+    let signal = s.to_uppercase();
+
+    let sig_num = SIGLIST
+        .iter()
+        .find(|(name, _)| name == &signal)
+        .map(|(_, num)| *num);
+
+    match sig_num {
+        Some(num) => Ok(num),
+        None => Err(format!("invalid signal name '{}'", s)),
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // parse command line arguments
-    let _args = Args::try_parse().unwrap_or_else(|err| {
-        err.source();
-        eprintln!(
-            "Error: {}",
-            err.source()
-                .map_or_else(|| err.kind().to_string(), |err| err.to_string())
-        );
-        std::process::exit(DEFAULT_ERROR_EXIT_STATUS);
+    let _args = Args::try_parse().unwrap_or_else(|err| match err.kind() {
+        clap::error::ErrorKind::DisplayHelp | clap::error::ErrorKind::DisplayVersion => {
+            print!("{err}");
+            std::process::exit(0);
+        }
+        _ => {
+            eprintln!(
+                "Error: {}",
+                err.source()
+                    .map_or_else(|| err.kind().to_string(), |err| err.to_string())
+            );
+            std::process::exit(DEFAULT_ERROR_EXIT_STATUS);
+        }
     });
 
     setlocale(LocaleCategory::LcAll, "");
