bc92771a6d271c9503d879a03cde71ec294e6f76
Dmytro Kriuchkov <kriuchkovdima@gmail.com>
2024-09-19 10:18:06 +0300

    Restructure

--- process/timeout.rs
+++ process/timeout.rs
@@ -7,26 +7,41 @@
 // SPDX-License-Identifier: MIT
 //
 
-extern crate clap;
-extern crate libc;
-extern crate plib;
-
 use clap::Parser;
 use gettextrs::{bind_textdomain_codeset, setlocale, textdomain, LocaleCategory};
 use nix::{
     errno::Errno,
-    sys::signal::{kill, Signal},
-    unistd::Pid,
+    sys::{
+        signal::{
+            raise, sigaction, signal, sigprocmask, SaFlags, SigAction, SigEvent, SigHandler,
+            SigSet, SigevNotify, SigmaskHow,
+            Signal::{self, SIGKILL, SIGTERM, SIGTTIN, SIGTTOU},
+        },
+        timer::{Expiration, Timer, TimerSetTimeFlags},
+        wait::{waitpid, WaitPidFlag, WaitStatus},
+    },
+    time::ClockId,
+    unistd::{execvp, fork, setpgid, ForkResult, Pid},
 };
 use plib::PROJECT_NAME;
 use std::{
     error::Error,
-    os::unix::process::ExitStatusExt,
-    process::{Child, Command, ExitStatus},
+    ffi::CString,
     str::FromStr,
-    thread,
-    time::{Duration, Instant},
+    sync::{
+        atomic::{AtomicBool, AtomicI32, Ordering},
+        Arc, LazyLock, Mutex,
+    },
+    time::Duration,
 };
+
+static FOREGROUND: LazyLock<Arc<AtomicBool>> = LazyLock::new(|| Arc::new(AtomicBool::new(false)));
+static FIRST_SIGNAL: LazyLock<Arc<AtomicI32>> =
+    LazyLock::new(|| Arc::new(AtomicI32::new(SIGTERM as i32)));
+static KILL_AFTER: LazyLock<Arc<Mutex<Option<Duration>>>> =
+    LazyLock::new(|| Arc::new(Mutex::new(None)));
+static MONITORED_PID: LazyLock<Arc<AtomicI32>> = LazyLock::new(|| Arc::new(AtomicI32::new(0)));
+static TIMED_OUT: LazyLock<Arc<AtomicBool>> = LazyLock::new(|| Arc::new(AtomicBool::new(false)));
 
 /// timeout — execute a utility with a time limit
 #[derive(Parser, Debug)]
@@ -49,7 +64,7 @@ struct Args {
     /// Specify the signal to send when the time limit is reached, using one of the symbolic names defined in the <signal.h> header.
     /// Values of signal shall be recognized in a case-independent fashion, without the SIG prefix. By default, SIGTERM shall be sent.
     #[arg(short = 's', long, default_value = "TERM", value_parser = parse_signal)]
-    signal: Signal,
+    signal_name: Signal,
 
     /// The maximum amount of time to allow the utility to run, specified as a decimal number with an optional decimal fraction and an optional suffix.
     #[arg(name = "DURATION", value_parser = parse_duration)]
@@ -98,133 +113,242 @@ fn parse_signal(s: &str) -> Result<Signal, String> {
     Signal::from_str(&signal_name).map_err(|_| format!("invalid signal name '{s}'"))
 }
 
-#[derive(thiserror::Error, Debug)]
-enum TimeoutError {
-    #[error("timeout reached")]
-    TimeoutReached,
-    #[error("signal sent '{0}'")]
-    SignalSent(ExitStatus),
-    #[error("{0}")]
-    Other(String),
-    #[error("unable to run the utility '{0}'")]
-    UnableToRunUtility(String),
-    #[error("utility '{0}' not found")]
-    UtilityNotFound(String),
+/// Starts the timeout after which [Signal::SIGALRM] will be send.
+fn set_timeout(duration: Duration) {
+    let sig_event = SigEvent::new(SigevNotify::SigevSignal {
+        signal: Signal::SIGALRM,
+        si_value: 0,
+    });
+
+    let mut timer = Timer::new(ClockId::CLOCK_MONOTONIC, sig_event).unwrap();
+    let expiration = Expiration::OneShot(duration.into());
+    let flags = TimerSetTimeFlags::empty();
+    timer.set(expiration, flags).expect("failed to set timer")
 }
 
-impl From<std::io::Error> for TimeoutError {
-    fn from(error: std::io::Error) -> Self {
-        TimeoutError::Other(error.to_string())
+/// Sends a signal to the process or process group.
+fn send_signal(pid: i32, signal: i32) {
+    if pid == 0 {
+        unsafe { libc::signal(signal, libc::SIG_IGN) };
+    }
+    unsafe {
+        libc::kill(pid, signal);
     }
 }
 
-impl From<Errno> for TimeoutError {
-    fn from(error: Errno) -> Self {
-        TimeoutError::Other(error.to_string())
-    }
-}
+extern "C" fn chld_handler(_signal: i32) {}
 
-impl From<TimeoutError> for i32 {
-    fn from(error: TimeoutError) -> Self {
-        match error {
-            TimeoutError::TimeoutReached => 124,
-            TimeoutError::SignalSent(signal) => 128 + signal.signal().unwrap(),
-            TimeoutError::Other(_) => 125,
-            TimeoutError::UnableToRunUtility(_) => 126,
-            TimeoutError::UtilityNotFound(_) => 127,
+extern "C" fn handler(mut signal: i32) {
+    if signal == libc::SIGALRM {
+        TIMED_OUT.store(true, Ordering::SeqCst);
+        signal = FIRST_SIGNAL.load(Ordering::SeqCst);
+    }
+    if 0 < MONITORED_PID.load(Ordering::SeqCst) {
+        if let Some(duration) = *KILL_AFTER.lock().unwrap() {
+            FIRST_SIGNAL.store(libc::SIGKILL, Ordering::SeqCst);
+            set_timeout(duration);
+            *KILL_AFTER.lock().unwrap() = Some(Duration::from_secs(0));
         }
-    }
-}
 
-fn spawn_child(utility: &str, arguments: &[String]) -> Result<Child, TimeoutError> {
-    Command::new(utility)
-        .args(arguments)
-        .spawn()
-        .map_err(|err| match err.kind() {
-            std::io::ErrorKind::NotFound => TimeoutError::UtilityNotFound(utility.to_string()),
-            std::io::ErrorKind::PermissionDenied => {
-                TimeoutError::UnableToRunUtility(utility.to_string())
+        send_signal(MONITORED_PID.load(Ordering::SeqCst), signal);
+
+        if !FOREGROUND.load(Ordering::SeqCst) {
+            send_signal(0, signal);
+            if signal != libc::SIGKILL && signal != libc::SIGCONT {
+                send_signal(MONITORED_PID.load(Ordering::SeqCst), libc::SIGCONT);
+                send_signal(0, libc::SIGCONT);
             }
-            _ => err.into(),
-        })
-}
-
-fn send_signal(pid: Pid, signal: Signal) -> Result<(), TimeoutError> {
-    kill(pid, signal).map_err(Into::into)
-}
-
-fn wait(child: &mut Child) -> Result<ExitStatus, TimeoutError> {
-    child.wait().map_err(Into::into)
-}
-
-fn wait_for_duration(child: &mut Child, duration: Duration) -> Result<ExitStatus, TimeoutError> {
-    if duration.is_zero() {
-        wait(child).map_err(Into::<TimeoutError>::into)
-    } else {
-        drop(child.stdin.take());
-
-        let start = Instant::now();
-        while start.elapsed() < duration {
-            if let Some(status) = child.try_wait().map_err(Into::<TimeoutError>::into)? {
-                return Ok(status);
-            }
-            thread::sleep(Duration::from_millis(100));
         }
-        Err(TimeoutError::TimeoutReached)
+    } else if 0 == MONITORED_PID.load(Ordering::SeqCst) {
+        std::process::exit(128 + signal);
     }
 }
 
-fn resolve_status(status: ExitStatus, preserve_status: bool) -> TimeoutError {
-    if preserve_status {
-        TimeoutError::SignalSent(status)
-    } else {
-        TimeoutError::TimeoutReached
+fn unblock_signal(signal: Signal) {
+    let mut sig_set = SigSet::empty();
+    sig_set.add(signal);
+    if sigprocmask(SigmaskHow::SIG_UNBLOCK, Some(&sig_set), None).is_err() {
+        eprintln!("timeout: failed to set unblock signals mask");
+        std::process::exit(125)
     }
 }
 
-fn timeout(args: Args) -> Result<ExitStatus, TimeoutError> {
+fn install_chld() {
+    let handler = SigHandler::Handler(chld_handler);
+    let flags = SaFlags::SA_RESTART;
+    let mask = SigSet::empty();
+    let sa = SigAction::new(handler, flags, mask);
+
+    unsafe {
+        let _ = sigaction(Signal::SIGCHLD, &sa);
+    };
+
+    unblock_signal(Signal::SIGCHLD);
+}
+
+fn install_handler(signal: Signal) {
+    let handler = SigHandler::Handler(handler);
+    let flags = SaFlags::SA_RESTART;
+    let mask = SigSet::empty();
+    let sa = SigAction::new(handler, flags, mask);
+
+    unsafe {
+        let _ = sigaction(Signal::SIGALRM, &sa);
+        let _ = sigaction(Signal::SIGINT, &sa);
+        let _ = sigaction(Signal::SIGQUIT, &sa);
+        let _ = sigaction(Signal::SIGHUP, &sa);
+        let _ = sigaction(Signal::SIGTERM, &sa);
+        let _ = sigaction(signal, &sa);
+    }
+}
+
+fn block_handler_and_chld(signal: Signal, old_set: &mut SigSet) {
+    let mut block_set = SigSet::empty();
+
+    block_set.add(Signal::SIGALRM);
+    block_set.add(Signal::SIGINT);
+    block_set.add(Signal::SIGQUIT);
+    block_set.add(Signal::SIGHUP);
+    block_set.add(Signal::SIGTERM);
+    block_set.add(signal);
+
+    block_set.add(Signal::SIGCHLD);
+
+    if sigprocmask(SigmaskHow::SIG_BLOCK, Some(&block_set), Some(old_set)).is_err() {
+        eprintln!("timeout: failed to set block signals mask");
+        std::process::exit(125)
+    }
+}
+
+fn disable_core_dumps() -> bool {
+    // TODO
+    true
+}
+
+fn timeout(args: Args) -> i32 {
     let Args {
+        foreground,
+        mut preserve_status,
         kill_after,
-        signal,
+        signal_name,
         duration,
         utility,
-        arguments,
-        preserve_status,
-        ..
+        mut arguments,
     } = args;
 
-    let mut child = spawn_child(&utility, &arguments)?;
-    let pid = Pid::from_raw(child.id() as libc::pid_t);
+    FOREGROUND.store(foreground, Ordering::SeqCst);
+    FIRST_SIGNAL.store(signal_name as i32, Ordering::SeqCst);
+    *KILL_AFTER.lock().unwrap() = kill_after;
 
-    match wait_for_duration(&mut child, duration) {
-        Ok(status) => Ok(status),
-        Err(TimeoutError::TimeoutReached) => {
-            // Send first signal
-            send_signal(pid, signal)?;
+    if !foreground {
+        if setpgid(Pid::from_raw(0), Pid::from_raw(0)).is_err() {
+            eprintln!("timeout: failed to set PGID");
+            return 125;
+        }
+    }
 
-            match kill_after {
-                Some(kill_duration) => {
-                    // Attempt to wait for process exit status again
-                    wait_for_duration(&mut child, kill_duration)
-                        .and_then(|status| Err(resolve_status(status, preserve_status)))
-                        .or_else(|err| {
-                            if let TimeoutError::TimeoutReached = err {
-                                // Send SIGKILL signal
-                                send_signal(pid, Signal::SIGKILL)?;
-                                let status = wait(&mut child)?;
-                                Err(resolve_status(status, preserve_status))
-                            } else {
-                                Err(err)
-                            }
-                        })
+    install_handler(signal_name);
+    unsafe {
+        if signal(SIGTTIN, SigHandler::SigIgn).is_err() {
+            eprintln!("timeout: failed to set SIGTTIN handler");
+            return 125;
+        }
+        if signal(SIGTTOU, SigHandler::SigIgn).is_err() {
+            eprintln!("timeout: failed to set SIGTTOU handler");
+            return 125;
+        }
+    }
+    install_chld();
+
+    unblock_signal(Signal::SIGALRM);
+
+    let mut sig_set = SigSet::empty();
+    block_handler_and_chld(signal_name, &mut sig_set);
+
+    match unsafe { fork() } {
+        Ok(ForkResult::Child) => {
+            // ============
+            // println!("CHILD!!!");
+            // ============
+
+            if sigprocmask(SigmaskHow::SIG_SETMASK, Some(&sig_set), None).is_err() {
+                eprintln!("timeout: failed to reset signal mask");
+                return 125;
+            }
+
+            unsafe {
+                if signal(SIGTTIN, SigHandler::SigIgn).is_err() {
+                    eprintln!("timeout: failed to set SIGTTIN handler");
+                    return 125;
                 }
-                None => {
-                    let status = wait(&mut child)?;
-                    Err(resolve_status(status, preserve_status))
+                if signal(SIGTTOU, SigHandler::SigIgn).is_err() {
+                    eprintln!("timeout: failed to set SIGTTOU handler");
+                    return 125;
+                }
+            }
+
+            let utility_c = CString::new(utility.clone()).unwrap();
+            let mut arguments_c: Vec<CString> = arguments
+                .drain(..)
+                .map(|arg| CString::new(arg).unwrap())
+                .collect();
+            arguments_c.insert(0, utility_c.clone());
+            match execvp(&utility_c, &arguments_c) {
+                Ok(_) => 0,
+                Err(Errno::ENOENT) => {
+                    eprintln!("timeout: utility '{utility}' not found");
+                    127
+                }
+                Err(_) => {
+                    eprintln!("timeout: unable to run the utility '{utility}'");
+                    126
                 }
             }
         }
-        Err(err) => Err(err),
+        Ok(ForkResult::Parent { child }) => {
+            // ============
+            // println!("PARENT!!!");
+            // ============
+
+            MONITORED_PID.store(child.as_raw(), Ordering::SeqCst);
+
+            set_timeout(duration);
+
+            let mut wait_status: WaitStatus;
+            loop {
+                wait_status = waitpid(child, Some(WaitPidFlag::WNOHANG)).unwrap();
+                if wait_status == WaitStatus::StillAlive {
+                    let _ = sig_set.suspend();
+                } else {
+                    break;
+                }
+            }
+            let status = match wait_status {
+                WaitStatus::Exited(_, status) => status,
+                WaitStatus::Signaled(_, rec_signal, core_dumped) => {
+                    if core_dumped {
+                        eprintln!("timeout: child has dumper the core")
+                    }
+                    if !TIMED_OUT.load(Ordering::SeqCst) && disable_core_dumps() {
+                        let _ = unsafe { signal(rec_signal, SigHandler::SigDfl) };
+                        unblock_signal(rec_signal);
+                        let _ = raise(rec_signal);
+                    }
+                    if TIMED_OUT.load(Ordering::SeqCst) && rec_signal == SIGKILL {
+                        preserve_status = true;
+                    }
+                    128 + rec_signal as i32
+                }
+                _ => 125,
+            };
+
+            if TIMED_OUT.load(Ordering::SeqCst) && !preserve_status {
+                124
+            } else {
+                status
+            }
+        }
+        Err(_) => 125,
     }
 }
 
@@ -249,16 +373,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     textdomain(PROJECT_NAME)?;
     bind_textdomain_codeset(PROJECT_NAME, "UTF-8")?;
 
-    let exit_code = match timeout(args) {
-        Ok(exit_status) => exit_status.code().unwrap(),
-        Err(err) => {
-            match err {
-                TimeoutError::TimeoutReached | TimeoutError::SignalSent(_) => {}
-                _ => eprintln!("timeout: {err}"),
-            }
-            err.into()
-        }
-    };
-
+    let exit_code = timeout(args);
     std::process::exit(exit_code);
 }
