3d9ba920d4d4846b8965df76bd43a12e9356bf74
Inkvisto <qwertuasdfgh953@gmail.com>
2024-09-23 09:28:08 +0300

    [talk] add comments licension

--- users/tests/talk/mod.rs
+++ users/tests/talk/mod.rs
@@ -1,3 +1,12 @@
+//
+// Copyright (c) 2024 Hemi Labs, Inc.
+//
+// This file is part of the posixutils-rs project covered under
+// the MIT License.  For the full license text, please see the LICENSE
+// file in the root directory of this project.
+// SPDX-License-Identifier: MIT
+//
+
 use std::ffi::CStr;
 use std::io;
 use std::net::{Ipv4Addr, SocketAddrV4, UdpSocket};
