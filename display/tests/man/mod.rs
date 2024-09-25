a7da87ee40f40617ebeb4d2496d2b53ef5227bc4
Dmytro Kriuchkov <kriuchkovdima@gmail.com>
2024-09-25 15:04:00 +0300

    Refactored tests

--- display/tests/man/mod.rs
+++ display/tests/man/mod.rs
@@ -7,70 +7,37 @@
 // SPDX-License-Identifier: MIT
 //
 
-use std::{
-    io::Write,
-    process::{Command, Output, Stdio},
-};
+use std::process::Output;
 
-use plib::TestPlan;
+use plib::{run_test_with_checker, TestPlan};
 
-/// Original [run_test_base](plib::testing::run_test_base) is private.
-fn run_test_base(cmd: &str, args: &Vec<String>, stdin_data: &[u8]) -> Output {
-    let relpath = if cfg!(debug_assertions) {
-        format!("target/debug/{}", cmd)
-    } else {
-        format!("target/release/{}", cmd)
-    };
-    let test_bin_path = std::env::current_dir()
-        .unwrap()
-        .parent()
-        .unwrap() // Move up to the workspace root from the current package directory
-        .join(relpath); // Adjust the path to the binary
+fn test_checker_man(plan: &TestPlan, output: &Output) {
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    assert!(stdout.contains(&plan.expected_out));
 
-    let mut command = Command::new(test_bin_path);
-    let mut child = command
-        .args(args)
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn()
-        .expect("failed to spawn head");
+    let stderr = String::from_utf8_lossy(&output.stderr);
+    assert_eq!(stderr, *plan.expected_err);
 
-    let stdin = child.stdin.as_mut().expect("failed to get stdin");
-    stdin
-        .write_all(stdin_data)
-        .expect("failed to write to stdin");
-
-    let output = child.wait_with_output().expect("failed to wait for child");
-    output
-}
-
-fn get_output(plan: TestPlan) -> Output {
-    run_test_base(&plan.cmd, &plan.args, plan.stdin_data.as_bytes())
+    assert_eq!(output.status.code(), Some(plan.expected_exit_code));
+    if plan.expected_exit_code == 0 {
+        assert!(output.status.success());
+    }
 }
 
 fn run_test_man(args: &[&str], expected_out: &str, expected_err: &str, expected_exit_code: i32) {
     let str_args: Vec<String> = args.iter().map(|s| String::from(*s)).collect();
 
-    let output = get_output(TestPlan {
-        cmd: String::from("man"),
-        args: str_args,
-        stdin_data: String::new(),
-        expected_out: String::from(expected_out),
-        expected_err: String::from(expected_err),
-        expected_exit_code,
-    });
-
-    let stdout = String::from_utf8_lossy(&output.stdout);
-    assert!(stdout.contains(expected_out));
-
-    let stderr = String::from_utf8_lossy(&output.stderr);
-    assert_eq!(stderr, expected_err);
-
-    assert_eq!(output.status.code(), Some(expected_exit_code));
-    if expected_exit_code == 0 {
-        assert!(output.status.success());
-    }
+    run_test_with_checker(
+        TestPlan {
+            cmd: String::from("man"),
+            args: str_args,
+            stdin_data: String::new(),
+            expected_out: String::from(expected_out),
+            expected_err: String::from(expected_err),
+            expected_exit_code,
+        },
+        test_checker_man,
+    );
 }
 
 #[test]
