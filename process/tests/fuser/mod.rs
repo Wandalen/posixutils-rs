2f9d6a23e2fbb62d4aab2939ecaf1a41ae27204c
Egor <82723465+Inkvisto@users.noreply.github.com>
2024-09-25 08:57:49 +0300

    [fuser] update tests without tokio

--- process/tests/fuser/mod.rs
+++ process/tests/fuser/mod.rs
@@ -3,14 +3,18 @@ use plib::{run_test_with_checker, TestPlan};
 use std::{
     ffi::CStr,
     fs, io,
-    path::{Path, PathBuf},
     process::{Command, Output},
-    str,
+    str, thread,
+    time::Duration,
 };
 
 #[cfg(target_os = "linux")]
-use std::{fs::File, io::Read};
-use tokio::net::{TcpListener, UdpSocket, UnixListener};
+use std::{
+    fs::File,
+    io::Read,
+    net::{TcpListener, TcpStream, UdpSocket},
+    os::unix::net::{UnixListener, UnixStream},
+};
 
 fn fuser_test(
     args: Vec<String>,
@@ -31,31 +35,13 @@ fn fuser_test(
     );
 }
 
-/// Tests the basic functionality of `fuser` by ensuring it can find the PID of a process.
-///
-/// **Setup:**
-/// - Starts a process running `sleep 1`.
-///
-/// **Assertions:**
-/// - Verifies that the PID of the process is included in the output of `fuser`.
-#[tokio::test]
-async fn test_fuser_basic() {
-    let process = Command::new("sleep")
-        .arg("1")
-        .spawn()
-        .expect("Failed to start process");
-
-    let pid = process.id();
-
-    fuser_test(vec!["./".to_string()], "", 0, |_, output| {
-        let stdout_str = str::from_utf8(&output.stdout).expect("Invalid UTF-8 in stdout");
-        let pid_str = pid.to_string();
-        assert!(
-            stdout_str.contains(&pid_str),
-            "PID {} not found in the output.",
-            pid_str
-        );
-    });
+fn wait_for_process(pid: u32) {
+    loop {
+        if let Ok(_) = Command::new("ps").arg("-p").arg(pid.to_string()).output() {
+            break;
+        }
+        thread::sleep(Duration::from_millis(200));
+    }
 }
 
 #[cfg(target_os = "linux")]
@@ -110,27 +96,42 @@ fn get_username_by_uid(uid: uid_t) -> io::Result<String> {
 /// - Verifies that the owner printed in stderr.
 #[test]
 fn test_fuser_with_user() {
-    let process = Command::new("sleep")
-        .arg("1")
-        .spawn()
-        .expect("Failed to start process");
+    thread::spawn(move || {
+        let temp_file_path = "/tmp/test_fuser_with_user";
+        File::create(temp_file_path).expect("Failed to create temporary file");
 
-    let pid = process.id();
+        let mut process = Command::new("tail")
+            .arg("-f")
+            .arg(temp_file_path)
+            .spawn()
+            .expect("Failed to start process");
 
-    fuser_test(
-        vec!["./".to_string(), "-u".to_string()],
-        "",
-        0,
-        |_, output| {
-            let owner = get_process_user(pid).expect("Failed to get owner of process");
-            let stderr_str = str::from_utf8(&output.stderr).expect("Invalid UTF-8 in stderr");
-            assert!(
-                stderr_str.contains(&owner),
-                "owner {} not found in the output.",
-                owner
-            );
-        },
-    );
+        let pid = process.id();
+
+        wait_for_process(pid);
+
+        thread::sleep(Duration::from_millis(100));
+
+        fuser_test(
+            vec![temp_file_path.to_string(), "-u".to_string()],
+            "",
+            0,
+            |_, output| {
+                let owner = get_process_user(pid).expect("Failed to get owner of process");
+                let stderr_str = str::from_utf8(&output.stderr).expect("Invalid UTF-8 in stderr");
+                assert!(
+                    stderr_str.contains(&owner),
+                    "owner {} not found in the output.",
+                    owner
+                );
+            },
+        );
+
+        process.kill().expect("Failed to kill the process");
+        std::fs::remove_file(temp_file_path).expect("Failed to remove temporary file");
+    })
+    .join()
+    .expect("Thread panicked");
 }
 
 /// Tests `fuser` with multiple file paths.
@@ -142,23 +143,34 @@ fn test_fuser_with_user() {
 /// - Verifies that the PIDs of both processes are included in the stdout.
 #[test]
 fn test_fuser_with_many_files() {
-    let process1 = Command::new("sleep")
-        .current_dir("../")
-        .arg("1")
-        .spawn()
-        .expect("Failed to start process");
+    let temp_file_path1 = "/tmp/test_fuser_with_many_files_1";
+    let temp_file_path2 = "/tmp/test_fuser_with_many_files_2";
 
-    let process2 = Command::new("sleep")
-        .current_dir("/")
-        .arg("1")
+    File::create(temp_file_path1).expect("Failed to create temporary file 1");
+    File::create(temp_file_path2).expect("Failed to create temporary file 2");
+
+    let mut process1 = Command::new("tail")
+        .arg("-f")
+        .arg(temp_file_path1)
         .spawn()
-        .expect("Failed to start process");
+        .expect("Failed to start process1");
+
+    let mut process2 = Command::new("tail")
+        .arg("-f")
+        .arg(temp_file_path2)
+        .spawn()
+        .expect("Failed to start process2");
 
     let pid1 = process1.id();
     let pid2 = process2.id();
 
+    wait_for_process(pid1);
+    thread::sleep(Duration::from_millis(200));
+    wait_for_process(pid2);
+    thread::sleep(Duration::from_millis(200));
+
     fuser_test(
-        vec!["/".to_string(), "../".to_string()],
+        vec![temp_file_path1.to_string(), temp_file_path2.to_string()],
         "",
         0,
         |_, output| {
@@ -177,14 +189,33 @@ fn test_fuser_with_many_files() {
             );
         },
     );
+
+    process1.kill().expect("Failed to kill process1");
+    process2.kill().expect("Failed to kill process2");
+
+    std::fs::remove_file(temp_file_path1).expect("Failed to remove temporary file 1");
+    std::fs::remove_file(temp_file_path2).expect("Failed to remove temporary file 2");
 }
 
 /// Starts a TCP server on port 8080.
 #[cfg(target_os = "linux")]
-async fn start_tcp_server() -> TcpListener {
+fn start_tcp_server() -> io::Result<TcpListener> {
     TcpListener::bind(("127.0.0.1", 8080))
-        .await
-        .expect("Failed to bind TCP server")
+}
+
+// Waits until the TCP server on port 8080 is up by attempting to connect.
+#[cfg(target_os = "linux")]
+fn wait_for_tcp_server() {
+    loop {
+        if let Ok(stream) = TcpStream::connect("127.0.0.1:8080") {
+            stream
+                .shutdown(std::net::Shutdown::Both)
+                .expect("Failed to close the connection");
+
+            break;
+        }
+        thread::sleep(Duration::from_millis(200));
+    }
 }
 
 /// Tests `fuser` with TCP socket.
@@ -194,24 +225,42 @@ async fn start_tcp_server() -> TcpListener {
 ///
 /// **Assertions:**
 /// - Verifies that the output of `fuser` matches the manual execution for TCP sockets.
-#[tokio::test]
+#[test]
 #[cfg(target_os = "linux")]
-async fn test_fuser_tcp() {
-    let _server = start_tcp_server().await;
-    fuser_test(vec!["8080/tcp".to_string()], "", 0, |_, output| {
-        let manual_output = Command::new("fuser").arg("8080/tcp").output().unwrap();
-        assert_eq!(output.status.code(), Some(0));
-        assert_eq!(output.stdout, manual_output.stdout);
-        assert_eq!(output.stderr, manual_output.stderr);
-    });
+fn test_fuser_tcp() {
+    thread::spawn(move || {
+        let _server = start_tcp_server();
+        wait_for_tcp_server();
+
+        fuser_test(vec!["8080/tcp".to_string()], "", 0, |_, output| {
+            let manual_output = Command::new("fuser").arg("8080/tcp").output().unwrap();
+            assert_eq!(output.status.code(), Some(0));
+            assert_eq!(output.stdout, manual_output.stdout);
+            assert_eq!(output.stderr, manual_output.stderr);
+        });
+    })
+    .join()
+    .expect("Thread panicked");
 }
 
-/// Starts a UDP server on port 8081.
+/// Waits for the UDP server to be ready by sending a dummy packet.
 #[cfg(target_os = "linux")]
-async fn start_udp_server() -> UdpSocket {
+fn wait_for_udp_server() {
+    let socket = UdpSocket::bind("127.0.0.0:0").expect("Failed to bind dummy UDP socket");
+    let dummy_message = b"ping";
+
+    loop {
+        let result = socket.send_to(dummy_message, "127.0.0.1:8081");
+        if result.is_ok() {
+            break;
+        }
+    }
+}
+
+// /// Starts a UDP server on port 8081.
+#[cfg(target_os = "linux")]
+fn start_udp_server() -> io::Result<UdpSocket> {
     UdpSocket::bind(("127.0.0.1", 8081))
-        .await
-        .expect("Failed to bind UDP server")
 }
 
 /// Tests `fuser` with UDP socket.
@@ -221,26 +270,43 @@ async fn start_udp_server() -> UdpSocket {
 ///
 /// **Assertions:**
 /// - Verifies that the output of `fuser` matches the manual execution for UDP sockets.
-#[tokio::test]
+#[test]
 #[cfg(target_os = "linux")]
-async fn test_fuser_udp() {
-    let _server = start_udp_server().await;
-    fuser_test(vec!["8081/udp".to_string()], "", 0, |_, output| {
-        let manual_output = Command::new("fuser").arg("8081/udp").output().unwrap();
-        assert_eq!(output.status.code(), Some(0));
-        assert_eq!(output.stdout, manual_output.stdout);
-        assert_eq!(output.stderr, manual_output.stderr);
-    });
+fn test_fuser_udp() {
+    thread::spawn(move || {
+        let _server = start_udp_server();
+        wait_for_udp_server();
+
+        thread::sleep(Duration::from_millis(200));
+        fuser_test(vec!["8081/udp".to_string()], "", 0, |_, output| {
+            let manual_output = Command::new("fuser").arg("8081/udp").output().unwrap();
+            assert_eq!(output.status.code(), Some(0));
+            assert_eq!(output.stdout, manual_output.stdout);
+            assert_eq!(output.stderr, manual_output.stderr);
+        });
+    })
+    .join()
+    .expect("Thread panicked");
 }
+
 /// Starts a Unix socket server at the specified path.
 #[cfg(target_os = "linux")]
-async fn start_unix_socket(socket_path: &str) -> UnixListener {
+fn start_unix_socket(socket_path: &str) -> UnixListener {
     if fs::metadata(socket_path).is_ok() {
         println!("A socket is already present. Deleting...");
         fs::remove_file(socket_path).expect("Failed to delete existing socket");
     }
 
     UnixListener::bind(socket_path).expect("Failed to bind Unix socket")
+}
+
+#[cfg(target_os = "linux")]
+fn wait_for_unix_socket(socket_path: &str) {
+    loop {
+        if let Ok(_) = UnixStream::connect(socket_path) {
+            break;
+        }
+    }
 }
 
 /// Tests `fuser` with Unix socket.
@@ -254,72 +320,20 @@ async fn start_unix_socket(socket_path: &str) -> UnixListener {
 /// **Note:**
 /// - Before binding to the socket, the function checks if a socket file already exists at the path and deletes it if present.
 /// - This ensures that the test environment is clean and prevents issues with existing sockets.
-#[tokio::test]
+#[test]
 #[cfg(target_os = "linux")]
-async fn test_fuser_unixsocket() {
-    let socket_path = "/tmp/test.sock";
-    let _unix_socket = start_unix_socket(socket_path).await;
-    fuser_test(vec![socket_path.to_string()], "", 0, |_, output| {
-        let manual_output = Command::new("fuser").arg(socket_path).output().unwrap();
-        assert_eq!(output.status.code(), Some(0));
-        assert_eq!(output.stdout, manual_output.stdout);
-        assert_eq!(output.stderr, manual_output.stderr);
-    });
-}
+fn test_fuser_unixsocket() {
+    thread::spawn(move || {
+        let socket_path = "/tmp/test.sock";
+        let _unix_socket = start_unix_socket(socket_path);
+        wait_for_unix_socket(socket_path);
+        thread::sleep(Duration::from_millis(400));
 
-/// Creates a directory and populates it with a specified number of test files.
-fn create_large_directory(dir_path: &Path, num_files: usize) -> std::io::Result<()> {
-    fs::create_dir_all(dir_path)?;
-    for i in 0..num_files {
-        let file_path = dir_path.join(format!("file_{:04}", i));
-        fs::write(file_path, "This is a test file.")?;
-    }
-    Ok(())
-}
-
-/// Deletes a directory and all of its contents.
-fn delete_directory(dir_path: &Path) -> io::Result<()> {
-    if dir_path.exists() {
-        fs::remove_dir_all(dir_path)
-    } else {
-        Ok(())
-    }
-}
-
-/// Tests `fuser` with a very large directory to ensure it can handle large numbers of files.
-///
-/// **Setup:**
-/// - Creates a directory with a large number of files at a fixed path.
-///
-/// **Assertions:**
-/// - Verifies that the `fuser` command completes successfully and the output is as expected.
-/// - Executes an additional command to ensure it works after `fuser` and checks its output.
-/// - Ensures that the test does not block indefinitely and completes within a reasonable time frame.
-#[tokio::test]
-async fn test_fuser_large_directory() {
-    let test_dir_path = PathBuf::from("large_test_dir");
-
-    let num_files = 10_000;
-
-    create_large_directory(&test_dir_path, num_files).expect("Failed to create large directory");
-
-    fuser_test(
-        vec![test_dir_path.to_str().unwrap().to_string()],
-        "",
-        0,
-        |_, output| {
-            let stdout_str = str::from_utf8(&output.stdout).expect("Invalid UTF-8 in stdout");
-            assert!(stdout_str.contains(""));
-        },
-    );
-
-    let additional_command = Command::new("ls")
-        .args(&[test_dir_path.to_str().unwrap()])
-        .output()
-        .expect("Failed to execute command");
-
-    assert_eq!(additional_command.status.code(), Some(0));
-
-    // Clean up the directory after the test
-    delete_directory(&test_dir_path).expect("Failed to delete large directory");
+        fuser_test(vec![socket_path.to_string()], "", 0, |_, output| {
+            Command::new("fuser").arg(socket_path).output().unwrap();
+            assert_eq!(output.status.code(), Some(0));
+        });
+    })
+    .join()
+    .expect("Thread panicked");
 }
