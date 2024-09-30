61802c057961a1f060ce3ccb6299defb754b1dab
Inkvisto <qwertuasdfgh953@gmail.com>
2024-09-30 08:41:42 +0300

    [newgrp] permissions checking and get current user

--- users/newgrp.rs
+++ users/newgrp.rs
@@ -1,15 +1,16 @@
-extern crate clap;
-extern crate gettextrs;
-extern crate plib;
-
 use clap::error::ErrorKind;
 use clap::Parser;
 use gettextrs::{bind_textdomain_codeset, setlocale, textdomain, LocaleCategory};
+use libc::{
+    endgrent, endpwent, getgrent, getgrgid, getgrnam, getlogin, getpwent, getpwnam, getpwuid,
+    getuid, group, passwd, setgrent, setpwent, MEMBARRIER_CMD_GLOBAL,
+};
+use plib::group::Group;
 use plib::PROJECT_NAME;
-
+use std::ffi::{CStr, CString};
 use std::{path::PathBuf, process};
 
-/// newgrp
+/// newgrp — change to a new group
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about)]
 struct Args {
@@ -19,11 +20,37 @@ struct Args {
 
     /// Specifies the group ID or group name. This is a positional argument that must be provided.
     #[arg(value_name = "GROUP", required = true)]
-    group: String,
+    group: PathBuf,
+}
+fn newgrp(args: Args) -> Result<(), &'static str> {
+    let groups = plib::group::load();
+    let group = find_matching_group("egor", groups);
+    Ok(())
 }
 
-fn newgrp(args: Args) -> Result<(), PatchError> {
-    Ok(())
+fn find_matching_group(name: &str, groups: Vec<Group>) -> Option<Group> {
+    for group in groups {
+        // Check if the user is a member of the current group
+        if group.members.iter().any(|member| member == name) {
+            // Return the group if the user is found as a member
+            return Some(group);
+        }
+    }
+    // Return None if no matching group is found
+    None
+}
+
+fn check_perms(grp: &libc::group, pwd: &mut libc::passwd, groupname: &str) {
+    let mut needspasswd = false;
+
+    // Check if user is a member of the group
+    if grp.gr_gid != pwd.pw_gid
+        && !is_on_list(grp.gr_mem(), unsafe {
+            CStr::from_ptr(pwd.pw_name).to_str().unwrap()
+        })
+    {
+        needspasswd = true;
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
