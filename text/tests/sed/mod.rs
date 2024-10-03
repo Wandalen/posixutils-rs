264f544a8643689238570a8c9f214bdd71b97760
Dmytro Kriuchkov <kriuchkovdima@gmail.com>
2024-10-03 11:58:20 +0300

    Tests Pt. 1

--- text/tests/sed/mod.rs
+++ text/tests/sed/mod.rs
@@ -27,3 +27,326 @@ fn sed_test(
         expected_exit_code,
     });
 }
+
+const ABC_INPUT: &'static str = "abc";
+const SCRIPT_A: &'static str = "s/a/ab/g";
+const SCRIPT_B: &'static str = "s/b/bc/g";
+const SCRIPT_C: &'static str = "s/c/ca/g";
+
+const ABC_FILE: &'static str = "tests/sed/abc";
+const CBA_FILE: &'static str = "tests/sed/cba";
+const SCRIPT_A_FILE: &'static str = "tests/sed/script_a";
+const SCRIPT_B_FILE: &'static str = "tests/sed/script_b";
+const SCRIPT_C_FILE: &'static str = "tests/sed/script_c";
+
+#[test]
+fn test_no_arguments() {
+    // TODO: implement and give own error message
+    // sed_test(&[], "", "", "sed: no scripts provided", 1);
+}
+
+#[test]
+fn test_single_script_input_stdin() {
+    sed_test(&[SCRIPT_A], ABC_INPUT, "abbc", "", 0);
+}
+
+#[test]
+fn test_single_script_input_file() {
+    sed_test(&[SCRIPT_A, ABC_FILE], "", "abbc", "", 0);
+}
+
+#[test]
+fn test_e_script_input_stdin() {
+    sed_test(&["-e", SCRIPT_A], ABC_INPUT, "abbc", "", 0);
+}
+
+#[test]
+fn test_e_script_input_file() {
+    sed_test(&["-e", SCRIPT_A, ABC_FILE], "", "abbc", "", 0);
+}
+
+#[test]
+fn test_f_script_input_stdin() {
+    sed_test(&["-f", SCRIPT_A_FILE], ABC_INPUT, "abbc", "", 0);
+}
+
+#[test]
+fn test_f_script_input_file() {
+    sed_test(&["-f", SCRIPT_A_FILE, ABC_FILE], "", "abbc", "", 0);
+}
+
+#[test]
+fn test_e_f_scripts_input_stdin() {
+    sed_test(
+        &["-e", SCRIPT_A, "-f", SCRIPT_B_FILE],
+        ABC_INPUT,
+        "abcbcc",
+        "",
+        0,
+    );
+}
+
+#[test]
+fn test_e_f_scripts_input_file() {
+    sed_test(
+        &["-e", SCRIPT_A, "-f", SCRIPT_B_FILE, ABC_FILE],
+        "",
+        "abcbcc",
+        "",
+        0,
+    );
+}
+
+#[test]
+fn test_input_explicit_stdin() {
+    sed_test(&[SCRIPT_A, "-"], ABC_INPUT, "abbc", "", 0);
+}
+
+#[test]
+fn test_ignore_stdin_without_dash() {
+    sed_test(&[SCRIPT_A, CBA_FILE], ABC_INPUT, "cbab", "", 0);
+}
+
+#[test]
+fn test_input_file_and_explicit_stdin() {
+    // Reorderind STDIN and input file
+    sed_test(&[SCRIPT_A, "-", CBA_FILE], ABC_INPUT, "abbc\ncbab", "", 0);
+    sed_test(&[SCRIPT_A, CBA_FILE, "-"], ABC_INPUT, "cbab\nabbc", "", 0);
+}
+
+#[test]
+fn test_single_script_multiple_input_files() {
+    // Reorderind input files
+    sed_test(&[SCRIPT_A, ABC_FILE, CBA_FILE], "", "abbc\ncbab", "", 0);
+    sed_test(&[SCRIPT_A, CBA_FILE, ABC_FILE], "", "cbab\nabbc", "", 0);
+}
+
+#[test]
+fn test_e_scripts_multiple_input_files() {
+    // Reorderind input files
+    sed_test(
+        &["-e", SCRIPT_A, "-e", SCRIPT_B, ABC_FILE, CBA_FILE],
+        "",
+        "abcbcc\ncbcabc",
+        "",
+        0,
+    );
+    sed_test(
+        &["-e", SCRIPT_A, "-e", SCRIPT_B, CBA_FILE, ABC_FILE],
+        "",
+        "cbcabc\nabcbcc",
+        "",
+        0,
+    );
+}
+
+#[test]
+fn test_e_scripts_multiple_input_files_mixed_order() {
+    // Reorderind input files
+    sed_test(
+        &[ABC_FILE, "-e", SCRIPT_A, CBA_FILE, "-e", SCRIPT_B],
+        "",
+        "abcbcc\ncbcabc",
+        "",
+        0,
+    );
+    sed_test(
+        &[CBA_FILE, "-e", SCRIPT_A, ABC_FILE, "-e", SCRIPT_B],
+        "",
+        "cbcabc\nabcbcc",
+        "",
+        0,
+    );
+}
+
+#[test]
+fn test_f_scripts_multiple_input_files() {
+    // Reorderind input files
+    sed_test(
+        &["-f", SCRIPT_A_FILE, "-f", SCRIPT_B_FILE, ABC_FILE, CBA_FILE],
+        "",
+        "abcbcc\ncbcabc",
+        "",
+        0,
+    );
+    sed_test(
+        &["-f", SCRIPT_A_FILE, "-f", SCRIPT_B_FILE, CBA_FILE, ABC_FILE],
+        "",
+        "cbcabc\nabcbcc",
+        "",
+        0,
+    );
+}
+
+#[test]
+fn test_f_scripts_multiple_input_files_mixed_order() {
+    // Reorderind input files
+    sed_test(
+        &[ABC_FILE, "-f", SCRIPT_A_FILE, CBA_FILE, "-f", SCRIPT_B_FILE],
+        "",
+        "abcbcc\ncbcabc",
+        "",
+        0,
+    );
+    sed_test(
+        &[CBA_FILE, "-f", SCRIPT_A_FILE, ABC_FILE, "-f", SCRIPT_B_FILE],
+        "",
+        "cbcabc\nabcbcc",
+        "",
+        0,
+    );
+}
+
+#[test]
+fn test_e_scripts_unique_order_unique_results() {
+    sed_test(
+        &["-e", SCRIPT_A, "-e", SCRIPT_B, "-e", SCRIPT_C],
+        ABC_INPUT,
+        "abcabcaca",
+        "",
+        0,
+    );
+    sed_test(
+        &["-e", SCRIPT_A, "-e", SCRIPT_C, "-e", SCRIPT_B],
+        ABC_INPUT,
+        "abcbcca",
+        "",
+        0,
+    );
+    sed_test(
+        &["-e", SCRIPT_B, "-e", SCRIPT_A, "-e", SCRIPT_C],
+        ABC_INPUT,
+        "abbcaca",
+        "",
+        0,
+    );
+    sed_test(
+        &["-e", SCRIPT_B, "-e", SCRIPT_C, "-e", SCRIPT_A],
+        ABC_INPUT,
+        "abbcabcab",
+        "",
+        0,
+    );
+    sed_test(
+        &["-e", SCRIPT_C, "-e", SCRIPT_A, "-e", SCRIPT_B],
+        ABC_INPUT,
+        "abcbccabc",
+        "",
+        0,
+    );
+    sed_test(
+        &["-e", SCRIPT_C, "-e", SCRIPT_B, "-e", SCRIPT_A],
+        ABC_INPUT,
+        "abbccab",
+        "",
+        0,
+    );
+}
+
+#[test]
+fn test_f_scripts_unique_order_unique_results() {
+    sed_test(
+        &[
+            "-f",
+            SCRIPT_A_FILE,
+            "-f",
+            SCRIPT_B_FILE,
+            "-f",
+            SCRIPT_C_FILE,
+        ],
+        ABC_INPUT,
+        "abcabcaca",
+        "",
+        0,
+    );
+    sed_test(
+        &[
+            "-f",
+            SCRIPT_A_FILE,
+            "-f",
+            SCRIPT_C_FILE,
+            "-f",
+            SCRIPT_B_FILE,
+        ],
+        ABC_INPUT,
+        "abcbcca",
+        "",
+        0,
+    );
+    sed_test(
+        &[
+            "-f",
+            SCRIPT_B_FILE,
+            "-f",
+            SCRIPT_A_FILE,
+            "-f",
+            SCRIPT_C_FILE,
+        ],
+        ABC_INPUT,
+        "abbcaca",
+        "",
+        0,
+    );
+    sed_test(
+        &[
+            "-f",
+            SCRIPT_B_FILE,
+            "-f",
+            SCRIPT_C_FILE,
+            "-f",
+            SCRIPT_A_FILE,
+        ],
+        ABC_INPUT,
+        "abbcabcab",
+        "",
+        0,
+    );
+    sed_test(
+        &[
+            "-f",
+            SCRIPT_C_FILE,
+            "-f",
+            SCRIPT_A_FILE,
+            "-f",
+            SCRIPT_B_FILE,
+        ],
+        ABC_INPUT,
+        "abcbccabc",
+        "",
+        0,
+    );
+    sed_test(
+        &[
+            "-f",
+            SCRIPT_C_FILE,
+            "-f",
+            SCRIPT_B_FILE,
+            "-f",
+            SCRIPT_A_FILE,
+        ],
+        ABC_INPUT,
+        "abbccab",
+        "",
+        0,
+    );
+}
+
+#[test]
+fn test_mixed_e_f_scripts() {
+    // -e script -f script -e script
+    sed_test(
+        &["-e", SCRIPT_A, "-f", SCRIPT_B_FILE, "-e", SCRIPT_C],
+        ABC_INPUT,
+        "abcabcaca",
+        "",
+        0,
+    );
+    // -f script -e script -f script
+    sed_test(
+        &["-f", SCRIPT_A_FILE, "-e", SCRIPT_C, "-f", SCRIPT_B_FILE],
+        ABC_INPUT,
+        "abcbcca",
+        "",
+        0,
+    );
+}
