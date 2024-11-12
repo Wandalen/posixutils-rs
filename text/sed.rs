90ea387bf08a526c2921c9dabb2e562fbfdb4d7b
Bilan Vadim <vadimb892@gmail.com>
2024-11-12 22:34:05 +0200

    Handle all commands in Script::parse and split Script::parse on parts

--- text/sed.rs
+++ text/sed.rs
@@ -8,11 +8,9 @@
 //
 
 use std::{
-    fs::File,
-    io::{BufRead, BufReader},
-    path::PathBuf, 
-    str::pattern::Pattern,
+    collections::HashMap, fs::File, io::{BufRead, BufReader}, path::PathBuf, str::pattern::Pattern
 };
+use libc::{regex_t, regcomp, regexec, REG_NOMATCH};
 
 use clap::Parser;
 use gettextrs::{bind_textdomain_codeset, gettext, setlocale, textdomain, LocaleCategory};
@@ -106,48 +104,81 @@ impl Args {
     }
 }
 
+/// 
 #[derive(thiserror::Error, Debug)]
 enum SedError {
     #[error("none script was supplied")]
     NoScripts,
     #[error("{0}")]
     Io(#[from] std::io::Error),
+    #[error("can't parse {}", .0)]
+    ParseError(String)
 }
 
+/// 
 struct Address(Vec<usize>);
 
+/// 
 enum SReplaceFlag{
-    N,
-    G,
-    P,
-    W(String)
+    /// 
+    ReplaceNth,                                   //n
+    /// 
+    ReplaceAll,                                   //g
+    /// 
+    PrintPatternIfReplace,                        //p
+    /// 
+    AppendToIfReplace(PathBuf)                     //w
 }
 
+/// 
 enum Command{
+    /// 
     Block(Address, Vec<Command>),                 // {
+    /// 
     PrintTextAfter(Address, String),              // a
+    /// 
     BranchToLabel(Address, Option<String>),       // b
+    /// 
     DeletePatternAndPrintText(Address, String),   // c
+    /// 
     DeletePattern(Address, bool),                 // d
+    /// 
     ReplacePatternWithHold(Address),              // g
+    /// 
     AppendHoldToPattern(Address),                 // G
+    /// 
     ReplaceHoldWithPattern(Address),              // h
+    /// 
     AppendPatternToHold(Address),                 // H
+    /// 
     PrintTextBefore(Address, String),             // i
+    /// 
     PrintPatternBinary(Address),                  // I
+    /// 
     NPrint(Address, bool),                        // nN?       
+    /// 
     PrintPattern(Address, bool),                  // pP
+    /// 
     Quit(Address),                                // q
+    /// 
     PrintFile(Address, PathBuf),                  // r
-    SReplace(Pattern, String, Vec<SReplaceFlag>), // s
-    Test(Address, String),                        // t
+    /// 
+    SReplace(regex_t, String, Vec<SReplaceFlag>), // s
+    /// 
+    Test(Address, Option<String>),                // t
+    /// 
     AppendPatternToFile(Address, PathBuf),        // w
+    /// 
     ExchangeSpaces(Address),                      // x
+    /// 
     YReplace(Address, String, String),            // y
+    /// 
     BearBranchLabel(String),                      // :
+    /// 
     PrintStandard(Address),                       // =
-    IgnoreComment,                                // #
-    Empty,                                        
+    /// 
+    IgnoreComment,                                // #                                       
+    /// 
     Unknown
 }
 
@@ -169,6 +200,7 @@ fn parse_address(chars: &[char], i: &mut usize, address: &mut Option<Address>) {
     }
 }
 
+/// 
 fn parse_text_attribute(chars: &[char], i: &mut usize) -> Option<String>{
     *i += 1;
     let Some(ch) = chars.get(i) else {
@@ -210,10 +242,232 @@ fn parse_text_attribute(chars: &[char], i: &mut usize) -> Option<String>{
     }
 }
 
+/// 
+fn parse_word_attribute(chars: &[char], i: &mut usize) -> Result<Option<String>, SedError>{
+    loop{
+        let ch = *chars[i];
+        match ch{
+            '\n' | ' ' | ';' => break,
+            '_' => {},
+            _ if ch.is_whitespace() || ch.is_control() || ch.is_ascii_punctuation() => {
+                return Err(SedError::ParseError("".to_string()));
+            },
+            _ => {}
+        }
+        i += 1;
+        if i < *chars.len(){
+            break;
+        }
+    }
+}
+
+/// 
+fn parse_path_attribute(chars: &[char], i: &mut usize) -> Result<PathBuf, SedError>{
+    try_next_blank(chars, i)?;
+    let start = *i; 
+    loop{
+        let ch = *chars[*i];
+        match ch{
+            '\n' | ' ' | ';' => {
+                *i += 1;
+                break;
+            },
+            '_' | '/' | '\\' | ':' => {},
+            _ if ch.is_whitespace() || ch.is_control() || ch.is_ascii_punctuation() => {
+                return Err(SedError::ParseError("".to_string()));
+            },
+            _ => {}
+        }
+        *i += 1;
+        if i >= *chars.len(){
+            break;
+        }
+    }
+
+    let rfile= PathBuf::from(chars[start..i]);
+    if rfile.exists() && rfile.is_file(){
+        Ok(rfile)
+    }else{
+        Err(SedError::ParseError(()))
+    }
+}
+
+///
+fn parse_block(chars: &[char], i: &mut usize) -> Result<(), SedError>{
+    let block_limits = chars.iter().enumerate().skip(*i)
+        .filter(|pair| pair.1 == '{' || pair.1 == '}')
+        .collect::<Vec<_>>();
+
+    let j = 1;
+    let k = 0;
+    loop{
+        match chars[k].1{
+            '{' => j += 1,
+            '}' => j -= 1,
+            _ => {}
+        }
+        if j <= 0{
+            break;
+        } 
+        k += 1;
+        if k >= block_limits.len(){
+            break;
+        }
+    }
+
+    if k < block_limits.len(){
+        match Script::parse(raw_script[(*i + 1)..block_limits[k].0]){
+            Ok(subscript) => commands.push(Command::Block(address, subscript.0)),
+            Err(err) => return Err(SedError::ParseError())
+        }
+    }else{
+        return Err(SedError::ParseError());
+    }
+    *i = k + 1;
+    Ok(())
+}
+
+fn parse_replace_command(chars: &[char], i: &mut usize) -> Result<(String, String), SedError>{
+    *i += 1;
+    let first_position= *i + 1;
+    let Some(splitter) = chars.get(*i) else {
+        return Err(SedError::ParseError(()));
+    };
+    *i += 1;
+    let splitters = chars.iter().enumerate().skip(*i)
+        .filter(|pair| pair.1 == splitter)
+        .map(|pair| pair.0)
+        .collect::<Vec<_>>();
+
+    if splitter == '/'{
+        splitters.retain(|j|
+            if let Some(previous_ch) = chars.get(s.checked_sub(1)){
+                previous_ch == '\\'
+            }else{
+                false
+            }
+        )
+    }
+
+    let Some(pattern) = raw_script.get(first_position..splitters[0]) else{
+        return Err(SedError::ParseError(()));
+    };
+
+    let Some(replacement) = raw_script.get((splitters[0] + 1)..splitters[1]) else{
+        return Err(SedError::ParseError(()));
+    };
+    *i = splitters[1] + 1;
+
+    Ok((pattern, replacement))
+}
+
+fn parse_s_flags(chars: &[char], i: &mut usize) -> Result<Vec<SReplaceFlag>, SedError>{
+    let mut flags = vec![];
+    let mut flag_map= HashMap::from([
+        ('n', 0),
+        ('g', 0),
+        ('p', 0),
+        ('w', 0),
+    ]);
+    let mut w_start_position = None;
+    while let Some(ch) = chars.get(*i){
+        match ch{
+            'n' => {
+                flag_map.get_mut(&'n').unwrap() += 1;
+                flags.push(SReplaceFlag::ReplaceNth);
+            },
+            'g' => {
+                flag_map.get_mut(&'g').unwrap() += 1;
+                flags.push(SReplaceFlag::ReplaceAll)
+            },
+            'p' => {
+                flag_map.get_mut(&'p').unwrap() += 1;
+                flags.push(SReplaceFlag::PrintPatternIfReplace)
+            },
+            'w' => {
+                if w_start_position.is_none(){
+                    w_start_position = Some(*i);
+                }
+                flag_map.get_mut(&'w').unwrap() += 1;
+                flags.push(SReplaceFlag::AppendToIfReplace("".to_string()))
+            },
+            _ => break
+        }
+        *i += 1;
+    }
+
+    let eq_w = |f| if let SReplaceFlag::AppendToIfReplace(_) = f{
+        true
+    }else { 
+        false 
+    };
+    let w_flags_position = flags.iter().position(eq_w);
+    let is_w_last = w_flags_position.unwrap() < (flags.len() - 1);
+    if (w_flags_position.is_some() && !is_w_last) 
+        || (flag_map.keys().any(|k| k > 1) && is_w_last){
+        return Err(SedError::ParseError(()));
+    }
+    if let Some(w_start_position) = w_start_position{
+        *i = w_start_position;
+        flags.resize_with(w_flags_position - 1, || SReplaceFlag::ReplaceNth);
+        let path = parse_path_attribute(chars, i)?;
+        flags.push(SReplaceFlag::AppendToIfReplace(path));
+    }
+    Ok(flags)
+}
+
+/// 
+fn try_next_blank(chars: &[char], i: &mut usize) -> Result<(), SedError>{
+    *i += 1;
+    let Some(ch) = chars.get(*i) else {
+        return Err(SedError::ParseError("".to_string()));
+    };
+
+    if ch != ' '{
+        return Err(SedError::ParseError("".to_string()));
+    }
+
+    Ok(())
+}
+
+/// Compiles [`pattern`] as [`regex_t`]
+fn compile_regex(pattern: String) -> Result<regex_t, SedError> {
+    #[cfg(target_os = "macos")]
+    let mut pattern = pattern.replace("\\\\", "\\");
+    #[cfg(all(unix, not(target_os = "macos")))]
+    let pattern = pattern.replace("\\\\", "\\");
+    let mut cflags = 0;
+
+    // macOS version of [regcomp](regcomp) from `libc` provides additional check
+    // for empty regex. In this case, an error
+    // [REG_EMPTY](https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man3/regcomp.3.html)
+    // will be returned. Therefore, an empty pattern is replaced with ".*".
+    #[cfg(target_os = "macos")]
+    {
+        pattern = if pattern == "" {
+            String::from(".*")
+        } else {
+            pattern
+        };
+    }
+
+    let c_pattern =
+        CString::new(pattern.clone()).map_err(|_| MoreError::StringParse(pattern.clone()))?;
+    let mut regex = unsafe { std::mem::zeroed::<regex_t>() };
+
+    if unsafe { regcomp(&mut regex, c_pattern.as_ptr(), cflags) } == 0 {
+        Ok(regex)
+    } else {
+        Err(SedError::ParseError(pattern))
+    }
+}
+
+/// 
 #[derive(Debug)] 
 struct Script(Vec<Command>);
 
 impl Script {
+    /// 
     fn parse(raw_script: impl AsRef<str>) -> Result<Script, SedError> {
         let mut commands = vec![];
         let mut address: Option<Address> = None;
@@ -228,23 +482,21 @@ impl Script {
                     parse_address(&chars, &mut i, &mut address);
                     continue;
                 },
-                ' ' => {},
-                '\n' => {},
-                '{' => {},
-                'a' => {
-                    if let Some(text) = parse_text_attribute(chars, &mut i){
-                        commands.push(Command::PrintTextAfter(address, text));
-                    }else{
-                        commands.push(Command::Unknown);
-                    }
+                ' ' | '\n' | ';' => {},
+                '{' => parse_block(chars, &mut i)?,
+                'a' => if let Some(text) = parse_text_attribute(chars, &mut i){
+                    commands.push(Command::PrintTextAfter(address, text));
+                }else{
+                    return Err(SedError::ParseError(()));
                 },
-                'b' => {},
-                'c' => {
-                    if let Some(text) = parse_text_attribute(chars, &mut i){
-                        commands.push(Command::DeletePatternAndPrintText(address, text));
-                    }else{
-                        commands.push(Command::Unknown);
-                    }
+                'b' => {
+                    try_next_blank(chars, &mut i)?;
+                    commands.push(Command::BranchToLabel(address, parse_word_attribute(&chars, &mut i)?));
+                },
+                'c' => if let Some(text) = parse_text_attribute(chars, &mut i){
+                    commands.push(Command::DeletePatternAndPrintText(address, text));
+                }else{
+                    return Err(SedError::ParseError(()));
                 },
                 'd' => commands.push(Command::DeletePattern(address, false)),
                 'D' => commands.push(Command::DeletePattern(address, true)),
@@ -252,12 +504,10 @@ impl Script {
                 'G' => commands.push(Command::AppendHoldToPattern(address)),
                 'h' => commands.push(Command::ReplaceHoldWithPattern(address)),
                 'H' => commands.push(Command::AppendPatternToHold(address)),
-                'i' => {
-                    if let Some(text) = parse_text_attribute(chars, &mut i){
-                        commands.push(Command::PrintTextBefore(address, text));
-                    }else{
-                        commands.push(Command::Unknown);
-                    }
+                'i' => if let Some(text) = parse_text_attribute(chars, &mut i){
+                    commands.push(Command::PrintTextBefore(address, text));
+                }else{
+                    return Err(SedError::ParseError(()));
                 },
                 'I' => commands.push(Command::PrintPatternBinary(address)),
                 'n' => commands.push(Command::NPrint(address, false)),
@@ -265,49 +515,28 @@ impl Script {
                 'p' => commands.push(Command::PrintPattern(address, false)),
                 'P' => commands.push(Command::PrintPattern(address, true)),
                 'q' => commands.push(Command::Quit(address)),
-                'r' => {},
+                'r' => commands.push(Command::PrintFile(address, parse_path_attribute(chars, &mut i)?)),
                 's' => {
-                    i += 1;
-                    let first_position= i + 1;
-                    let Some(splitter) = chars.get(i) else {
-                        break;
-                    };
-                    i += 1;
-                    let splitters = chars.iter().enumerate().skip(i)
-                        .filter(|pair| pair.1 == splitter)
-                        .map(|pair| pair.0)
-                        .collect::<Vec>();
-
-                    if splitter == '/'{
-                        splitters.retain(|j|
-                            if let Some(previous_ch) = chars.get(s.checked_sub(1)){
-                                previous_ch == '\\'
-                            }else{
-                                false
-                            }
-                        )
-                    }
-
-                    let Some(pattern) = raw_script.get(first_position..splitters[0]) else{
-                        commands.push(Command::Unknown);
-                        break;
-                    };
-
-                    let Some(replacement) = raw_script.get((splitters[0] + 1)..splitters[1]) else{
-                        commands.push(Command::Unknown);
-                        break;
-                    };
+                    let (pattern, replacement)= parse_replace_command(chars, &mut i)?;
                     let pattern = pattern;
-                    commands.push(Command::SReplace(pattern, replacement.to_owned(), ()));
+                    let re = compile_regex(pattern)?;
+                    let flags = parse_s_flags(chars, &mut i)?;
+                    commands.push(Command::SReplace(re, replacement.to_owned(), flags));
                 },
-                't' => {},
-                'w' => {},
+                't' => {
+                    try_next_blank(chars, &mut i)?;
+                    commands.push(Command::Test(address, parse_word_attribute(chars, &mut i)?));
+                },
+                'w' => commands.push(Command::AppendPatternToFile(address, parse_path_attribute(chars, &mut i)?)),
                 'x' => commands.push(Command::ExchangeSpaces(address)),
-                'y' => {},
-                ':' => {},
+                'y' => {
+                    let (pattern, replacement)= parse_replace_command(chars, &mut i)?;
+                    commands.push(Command::YReplace(address, string1, string2));
+                },
+                ':' => commands.push(Command::BearBranchLabel(parse_word_attribute(chars, &mut i)?)),
                 '=' => commands.push(Command::PrintStandard(address)),
                 '#' => commands.push(Command::IgnoreComment),
-                _ => commands.push(Command::Unknown)
+                _ => return Err(err)
             }
             i += 1;
         }
@@ -316,6 +545,7 @@ impl Script {
     }
 }
 
+/// 
 #[derive(Debug)] // TODO: debug only
 struct Sed {
     ere: bool,
@@ -328,6 +558,7 @@ struct Sed {
 }
 
 impl Sed {
+    /// 
     fn execute(&mut self, command: Command, line: &str) -> Result<(), SedError> {
         match command{
             Block(address, commands) => {},                     // {
@@ -352,12 +583,12 @@ impl Sed {
             YReplace(address, string1, string2) => {},          // y
             BearBranchLabel(label) => {},                       // :
             PrintStandard(address) => {},                       // =
-            IgnoreComment => {},                       // #
-            Empty => {},                                        
+            IgnoreComment => {},                       // #                                       
             Unknown => {}
         }
     }
 
+    /// 
     fn process_line(&mut self, line: &str) -> Result<(), SedError> {
         if !self.quiet {
             for command in self.script.0{
@@ -368,6 +599,7 @@ impl Sed {
         Ok(())
     }
 
+    /// 
     fn process_input(&mut self, mut reader: Box<dyn BufRead>) -> Result<(), SedError> {
         self.pattern_space.clear();
         self.hold_space.clear();
@@ -400,6 +632,7 @@ impl Sed {
         Ok(())
     }
 
+    /// 
     fn sed(&mut self) -> Result<(), SedError> {
         println!("SED: {self:?}");
 
@@ -469,41 +702,4 @@ let has_match = if is_not {
 } else {
     has_match != REG_NOMATCH
 };
-
-let re = compile_regex(pattern, self.args.case_insensitive)?;
-
-/// Compiles [`pattern`] as [`regex_t`]
-fn compile_regex(pattern: String, ignore_case: bool) -> Result<regex_t, MoreError> {
-    #[cfg(target_os = "macos")]
-    let mut pattern = pattern.replace("\\\\", "\\");
-    #[cfg(all(unix, not(target_os = "macos")))]
-    let pattern = pattern.replace("\\\\", "\\");
-    let mut cflags = 0;
-    if ignore_case {
-        cflags |= REG_ICASE;
-    }
-
-    // macOS version of [regcomp](regcomp) from `libc` provides additional check
-    // for empty regex. In this case, an error
-    // [REG_EMPTY](https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man3/regcomp.3.html)
-    // will be returned. Therefore, an empty pattern is replaced with ".*".
-    #[cfg(target_os = "macos")]
-    {
-        pattern = if pattern == "" {
-            String::from(".*")
-        } else {
-            pattern
-        };
-    }
-
-    let c_pattern =
-        CString::new(pattern.clone()).map_err(|_| MoreError::StringParse(pattern.clone()))?;
-    let mut regex = unsafe { std::mem::zeroed::<regex_t>() };
-
-    if unsafe { regcomp(&mut regex, c_pattern.as_ptr(), cflags) } == 0 {
-        Ok(regex)
-    } else {
-        Err(MoreError::StringParse(pattern))
-    }
-}
 */
