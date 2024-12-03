db10d71e52c9b240b2a2fe317fbddcf6a83e283f
Ivan "ukrustacean" Dereviankin <ukrustacean@gmail.com>
2024-12-03 08:53:47 +0100

    [make] No args tests

--- cron/tests/crontab.rs
+++ cron/tests/crontab.rs
@@ -3,6 +3,6 @@ use std::process::{Command, Stdio};
 #[test]
 fn no_args() {
     let mut command = Command::new("bash");
-    let result = command.args(&["-c", "./target/debug/cron"]).spawn().unwrap().wait().unwrap();
+    let result = command.args(&["-c", "./target/debug/crontab"]).spawn().unwrap().wait().unwrap();
     assert_eq!(result.code().unwrap(), 1);
 }
