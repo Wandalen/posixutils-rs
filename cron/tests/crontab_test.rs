4280261f9f7aaf0fe2df3f9db5be215b5dbeeb2f
Ivan "ukrustacean" Dereviankin <ukrustacean@gmail.com>
2024-12-13 14:10:08 +0100

    [cron] Other tests too

--- cron/tests/crontab_test.rs
+++ cron/tests/crontab_test.rs
@@ -59,48 +59,25 @@ fn no_args() {
 
 #[test]
 fn dash_e() {
-    let mut command = Command::new("bash");
-    let result = command
-        .args(["-c", "../target/debug/crontab -e"])
-        .spawn()
-        .unwrap()
-        .wait()
-        .unwrap();
-    assert_eq!(result.code().unwrap(), 1);
+
+    let output = run_test_base("crontab", &vec!["-e".to_string()], b"");
+    assert_eq!(output.status.code(), Some(1));
 }
 
 #[test]
 fn dash_l() {
-    let mut command = Command::new("bash");
-    let result = command
-        .args(["-c", "../target/debug/crontab -l"])
-        .spawn()
-        .unwrap()
-        .wait()
-        .unwrap();
-    assert_eq!(result.code().unwrap(), 0);
+    let output = run_test_base("crontab", &vec!["-l".to_string()], b"");
+    assert_eq!(output.status.code(), Some(0));
 }
 
 #[test]
 fn dash_r() {
-    let mut command = Command::new("bash");
-    let result = command
-        .args(["-c", "../target/debug/crontab -r"])
-        .spawn()
-        .unwrap()
-        .wait()
-        .unwrap();
-    assert_eq!(result.code().unwrap(), 1);
+    let output = run_test_base("crontab", &vec!["-r".to_string()], b"");
+    assert_eq!(output.status.code(), Some(1));
 }
 
 #[test]
 fn too_many_args() {
-    let mut command = Command::new("bash");
-    let result = command
-        .args(["-c", "../target/debug/crontab -erl"])
-        .spawn()
-        .unwrap()
-        .wait()
-        .unwrap();
-    assert_eq!(result.code().unwrap(), 1);
+    let output = run_test_base("crontab", &vec!["-erl".to_string()], b"");
+    assert_eq!(output.status.code(), Some(1));
 }
